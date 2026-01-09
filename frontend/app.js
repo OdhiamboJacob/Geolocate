3de9aa27bff74df108ce7962c0e5ae94e3261bd8
JACOB ODHIAMBO <61450363+OdhiamboJacob@users.noreply.github.com>
2026-01-09 13:35:54 +0000

    Geolocate3

--- frontend/app.js
+++ frontend/app.js
@@ -95,7 +95,7 @@ document.getElementById("weatherBtn").addEventListener("click", async () => {
 
   try {
     const res = await fetch(
-      `https://solid-eureka-756pp9j9xvj3p4wp-3000.app.github.dev/api/weather?lat=${currentCoords.latitude}&lon=${currentCoords.longitude}`
+      `https://geolocate-qy66.onrender.com/api/weather?lat=${currentCoords.latitude}&lon=${currentCoords.longitude}`
     )
 
     if (!res.ok) throw new Error("Weather backend error")
@@ -169,7 +169,7 @@ hotelsBtn.addEventListener("click", async () => {
 
   try {
     const res = await fetch(
-      `https://solid-eureka-756pp9j9xvj3p4wp-3000.app.github.dev/api/hotels?lat=${userLat}&lon=${userLon}`
+      `https://geolocate-qy66.onrender.com/api/hotels?lat=${userLat}&lon=${userLon}`
     )
     const hotels = await res.json()
 
